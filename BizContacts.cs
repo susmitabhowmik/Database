625ed484b674660964e2eb189e78bbb3e09bba7f
susmitabhowmik <susmita.bhowmik516@gmail.com>
2019-08-13 14:46:00 -0400

    Rename form1 to main and create biz contacts form. Rearrange MdiLayout of child elements to cascade, vertical layout, and horizontal layout within parent element

--- BizContacts.cs
+++ BizContacts.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Database
 {
-    public partial class Form1 : Form
+    public partial class BizContacts : Form
     {
-        public Form1()
+        public BizContacts()
         {
             InitializeComponent();
         }
---
+++ Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Database
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void BusinessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BizContacts form = new BizContacts(); //new business contacts form
+            form.MdiParent = this; // set the main form as the parent of each business form
+            form.Show(); // show the business contact form
+        }
+
+        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade); //cascade the child form within the main form
+        }
+
+        private void TileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical); //apply vertical layout to child forms within parent forms
+        }
+
+        private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal); //apply horizontal layout to children elements
+        }
+    }
+}
